9247b40aacfbe44db324dca91fd1fb811ff27449
Jose Garcia <jg03298@gmail.com>
2024-06-22 11:38:14 -0700

    Initial project setup

---
+++ src/main/java/org/soulos/notesrestapi/NotesResTapiApplication.java
@@ -0,0 +1,14 @@
+package org.soulos.notesrestapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NotesResTapiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NotesResTapiApplication.class, args);
+        System.out.println("Hello World!");
+    }
+
+}
