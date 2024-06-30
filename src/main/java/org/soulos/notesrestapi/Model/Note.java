cb6a0e2a1ff4289757576debe289228b29e6668a
Jose Garcia <jg03298@gmail.com>
2024-06-29 19:33:36 -0700

    Implemented custom deserializer intended to trim incoming strings

--- src/main/java/org/soulos/notesrestapi/Model/Note.java
+++ src/main/java/org/soulos/notesrestapi/Model/Note.java
@@ -1,19 +1,28 @@
 package org.soulos.notesrestapi.Model;
 
+import com.fasterxml.jackson.databind.annotation.JsonDeserialize;
+
 public class Note {
-    String noteId;
-    String noteContent;
+    @JsonDeserialize(using= Deserializer.class)
+    private String noteId;
+    private String noteContent;
+
+    public Note(){
+        noteId = null;
+        noteContent = null;
+    }
+
 
     public Note(String noteId, String noteContent) {
         this.noteId = noteId;
         this.noteContent = noteContent;
     }
 
-    public String getNoteName() {
+    public String getNoteId() {
         return noteId;
     }
 
-    public void setNoteName(String noteId) {
+    public void setNoteId(String noteId) {
         this.noteId = noteId;
     }
 
